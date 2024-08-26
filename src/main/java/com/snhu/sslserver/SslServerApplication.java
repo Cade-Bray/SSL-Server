48f1e2bcb64664391c5d323b40f7fc4192f01719
Cade <bray.cade@gmail.com>
2024-08-26 00:57:58 -0600

    final push

--- src/main/java/com/snhu/sslserver/SslServerApplication.java
+++ src/main/java/com/snhu/sslserver/SslServerApplication.java
@@ -2,6 +2,10 @@ package com.snhu.sslserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication
 public class SslServerApplication {
@@ -12,3 +16,23 @@ public class SslServerApplication {
 
 }
 //FIXME: Add route to enable check sum return of static data example:  String data = "Hello World Check Sum!";
+@RestController
+class ServerController{
+	@RequestMapping("/hash")
+	public String myHash() throws NoSuchAlgorithmException {
+		String data = "Hello World Check Sum!";
+
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(data.getBytes());
+		byte[] digest = md.digest();
+		StringBuilder sb = new StringBuilder();
+		for (byte b : digest) {
+			sb.append(String.format("%02x", b));
+		}
+
+		return "<p>Cade Bray</p>" +
+				"<p>data: " + data + "</p>" +
+				"<p>Name of Cipher Algorithm used: SHA-256</p>" +
+				"<p>Checksum Value:" + sb.toString() + "</p>";
+	}
+}
